2ffa0ef23a52d7f776765d986741a6a3a9273a4e
arthurnovaisdev <arthurnovaispeixoto@gmail.com>
2026-08-20 11:43:35 -0300

    Fist class: Position

---
+++ src/main/java/com/arthurnovaisdev/app/Main.java
@@ -0,0 +1,12 @@
+package com.arthurnovaisdev.app;
+
+import com.arthurnovaisdev.boardgame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Position position = new Position(3, 5);
+        System.out.println(position);
+
+    }
+}
